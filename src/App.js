a61f3a532674f54f5c218743977523431e0a3454
tikaps <tikapuspita92@gmail.com>
2023-09-25 13:48:49 +0200

    css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>APP</div>;
+};
 
-export default App
+export default App;
